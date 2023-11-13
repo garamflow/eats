d7c04edf21c7438ee0293d6efd6ce82a89c6ff2a
garamflow <garamflow@gmail.com>
2023-11-13 23:20:26 +0900

    [Feat] Service 인터페이스 구현

--- src/main/java/com/eats/eats/food/service/FoodService.java
+++ src/main/java/com/eats/eats/food/service/FoodService.java
@@ -1,4 +1,14 @@
 package com.eats.eats.food.service;
 
-public class FoodService {
+import com.eats.eats.food.Food;
+
+import java.util.Collection;
+import java.util.Optional;
+
+public interface FoodService {
+    Collection<Food> getAllFoods();
+    Optional<Food> getFoodById(Long id);
+    Food addFood(Long id, Food food);
+    Food updateFood(Long id, Food food);
+    void deleteFood(Long id);
 }
