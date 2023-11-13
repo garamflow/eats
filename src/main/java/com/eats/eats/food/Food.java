462979e661a1b683d2168eee1ddfa15a79237ed5
garamflow <garamflow@gmail.com>
2023-11-13 21:16:56 +0900

    [Refactor] Food 클래스 수정

--- src/main/java/com/eats/eats/food/Food.java
+++ src/main/java/com/eats/eats/food/Food.java
@@ -1,14 +1,30 @@
 package com.eats.eats.food;
 
+import java.util.Date;
+
 public class Food {
+    private Long id;
     private String name;
     private int price;
-    private String foodDescription;
+    private String description;
+    private Date createdDate;
+    private Date updatedDate;
 
-    public Food(String name, int price, String foodDescription) {
+    public Food(Long id, String name, int price, String description, Date createdDate, Date updatedDate) {
+        this.id = id;
         this.name = name;
         this.price = price;
-        this.foodDescription = foodDescription;
+        this.description = description;
+        this.createdDate = createdDate;
+        this.updatedDate = updatedDate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getName() {
@@ -27,11 +43,27 @@ public class Food {
         this.price = price;
     }
 
-    public String getFoodDescription() {
-        return foodDescription;
+    public String getDescription() {
+        return description;
     }
 
-    public void setFoodDescription(String foodDescription) {
-        this.foodDescription = foodDescription;
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public Date getUpdatedDate() {
+        return updatedDate;
+    }
+
+    public void setUpdatedDate(Date updatedDate) {
+        this.updatedDate = updatedDate;
     }
 }
