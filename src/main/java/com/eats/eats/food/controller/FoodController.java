df852c7233fd3f6be97ed7a05edc0ecfe2309801
garamflow <garamflow@gmail.com>
2023-11-13 23:18:53 +0900

    [Refactor] Food 컨트롤러 구현하기

--- src/main/java/com/eats/eats/food/controller/FoodController.java
+++ src/main/java/com/eats/eats/food/controller/FoodController.java
@@ -1,44 +1,49 @@
 package com.eats.eats.food.controller;
 
 import com.eats.eats.food.Food;
-import com.eats.eats.food.service.FoodService;
+import com.eats.eats.food.service.FoodServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import java.util.Collection;
+import java.util.Optional;
 
 @RestController
 public class FoodController {
     @Autowired
-    private final FoodService foodService;
+    private final FoodServiceImpl foodServiceImpl;
+
+    public FoodController(FoodServiceImpl foodServiceImpl) {
+        this.foodServiceImpl = foodServiceImpl;
+    }
 
     // 상품 전체 조회
     @GetMapping("/foods")
-    public List<Food> foods() {
-        return foodService.getAllFoods();
+    public Collection<Food> getAllFoods() {
+        return foodServiceImpl.getAllFoods();
     }
 
     // 상품 하나 상세 조회
     @GetMapping("/foods/{id}")
-    public Food getFood(@PathVariable Long id) {
-        return foodService.getFood(id);
+    public Optional<Food> getFoodById(@PathVariable Long id) {
+        return foodServiceImpl.getFoodById(id);
     }
 
     // 상품 등록
     @PostMapping("/foods/{id}")
-    public Food addFood(@PathVariable Long id, @PathVariable Food food) {
-        return foodService.addFood(id, food);
+    public Food addFood(@PathVariable Long id,@PathVariable Food food) {
+        return foodServiceImpl.addFood(id, food);
     }
 
     // 상품 수정
     @PutMapping("/foods/{id}")
     public Food updateFood(@PathVariable Long id, @PathVariable Food food) {
-        return foodService.updateFood(id, food);
+        return foodServiceImpl.updateFood(id, food);
     }
 
     // 상품 삭제
     @DeleteMapping("/foods/{id}")
-    public Food deleteFood(@PathVariable Long id) {
-        return foodService.deleteFood(id);
+    public void deleteFood(@PathVariable Long id) {
+        foodServiceImpl.deleteFood(id);
     }
 }
